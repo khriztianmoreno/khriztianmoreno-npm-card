6eeaba8a623ed74d864fbf0eacd8bdaaf7917412
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-08-03 11:44:13 -0500

    feat: update card

--- bin/card.js
+++ bin/card.js
@@ -2,8 +2,8 @@
 // 👆 Used to tell Node.js that this is a CLI tool
 
 // Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
+import chalk from 'chalk';
+import boxen from 'boxen';
 
 // Define options for Boxen
 const options = {
@@ -14,22 +14,21 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Cristian Moreno / khriztianmoreno 👨‍💻 💻 🔻'),
-  bio: chalk.white.bold('FullStack Javascript Developer and Community builder. Developer Avocado 🥑 | #React & #Vue Lover 👨🏻‍💻 ♥️ 👨🏻‍🏫 Co-Organize of @MedellinJS @avanet'),
+  name: chalk.white('Cristian Moreno / khriztianmoreno 👨🏼‍💻 💻 🔻'),
+  bio: chalk.white.bold(`I help others learn by doing through articles, videos, and courses about Javascript, React, and the static web.
+Microsoft MVP | Google Developer Expert | AWS Community Builder | Cloudinary Media Developer Expert`),
   handle: chalk.cyan('@khriztianmoreno'),
-  work: chalk.white('Frontend Developer - Mattersupply'),
+  work: chalk.white('Developer & Educator - Mattersupply'),
   twitter: chalk.cyan('https://twitter.com/khriztianmoreno'),
   github: chalk.cyan('https://github.com/khriztianmoreno'),
   linkedin: chalk.cyan('https://www.linkedin.com/in/khriztianmoreno/'),
   web: chalk.cyan('https://khriztianmoreno.com/'),
-  podcast: chalk.cyan('https://twitter.com/commitfm'),
   npx: chalk.white('npx khriztianmoreno'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
   labelLinkedIn: chalk.white.bold('  LinkedIn:'),
   labelWeb: chalk.white.bold('       Web:'),
-  labelPodcast: chalk.white.bold('       Podcast:'),
   labelCard: chalk.white.bold('      Card:')
 }
 
@@ -42,10 +41,9 @@ const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
-const podcasting = `${data.labelPodcast}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + bio + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + podcasting + carding
+const output = heading + newline + bio + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + carding
 
 console.log(chalk.green(boxen(output, options)))
